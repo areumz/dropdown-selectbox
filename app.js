8d1eb67f609daf58b32e91a3fde910430fd3e0fa
iRRPL-AR <ksah1016@naver.com>
2022-05-12 22:43:46 +0900

    add javascript

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const toggleBtn = document.querySelector('.dropdown-toggle')
 const list = document.querySelector('.dropdown-list')
-const options = document.querySelectorAll('dropdown-option')
+const python = document.querySelector('.python')
+const java = document.querySelector('.java')
+const js = document.querySelector('.js')
+const csharp = document.querySelector('.csharp')
+const c = document.querySelector('.c')
 
 toggleBtn.addEventListener('click', function() {
     list.classList.toggle('hidden')
@@ -10,5 +14,13 @@ toggleBtn.addEventListener('click', function() {
 toggleBtn.addEventListener('blur', function() {
     list.classList.add('hidden')
     toggleBtn.classList.remove('on')
+})
+
+const langArr = [python, java, js, csharp, c];
+
+langArr.forEach((item)=>{
+    item.addEventListener('mousedown', (event)=>{
+        toggleBtn.textContent = event.target.textContent;
+    })
 })
 
